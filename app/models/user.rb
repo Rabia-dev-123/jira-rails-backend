53cbbc7ce4ea4513a12cb87e7448d891e50aace2
Rabia <rabii.awajid@gmail.com>
2026-02-04 00:03:09 +0500

    changing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,23 @@
-
 class User < ApplicationRecord
-    has_secure_password
-    has_many :boards, dependent: :destroy
-  has_many :tasks
-  validates :email, presence: true, uniqueness: true
-  validates :name, presence: true
+  has_secure_password
 
-enum :role, { user: 0, admin: 1 }
+  has_many :boards, dependent: :destroy
+  has_many :tasks
+
+  # Email validation
+  validates :email, presence: true, uniqueness: true
+
+  # Username validation (8 to 20 characters)
+  validates :name, presence: true, length: { minimum: 8, maximum: 20 }
+
+  # Password validation (8 to 20 characters)
+  validates :password, length: { minimum: 8, maximum: 20 }, if: :password_present?
+
+  enum :role, { user: 0, admin: 1 }
+
+  private
+
+  def password_present?
+    password.present?
+  end
 end
